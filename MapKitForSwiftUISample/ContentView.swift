b1e4cbf17d97a7c5069a1ae6bb7fedb7c8808697
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-11 15:24:11 +0900

    [Add]: add new project.

---
+++ MapKitForSwiftUISample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapKitForSwiftUISample
+//
+//  Created by 白数叡司 on 2024/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKitForSwiftUISample/MapKitForSwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitForSwiftUISampleApp.swift
+//  MapKitForSwiftUISample
+//
+//  Created by 白数叡司 on 2024/04/11.
+//
+
+import SwiftUI
+
+@main
+struct MapKitForSwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
